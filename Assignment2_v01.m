0b0f9102e058f19cce2b3926f1a687d13b638e9d
Jiangu98 <liam.jiang-1@student.uts.edu.au>
2022-10-15 00:02:27 +1100

    classdefs

--- Assignment2_v01.m
+++ Assignment2_v01.m
@@ -1,46 +1,61 @@
+%%
+% 1. DH-params for robots. Model 2nd robot
+
+% 2. Include a GUI. Individual joint movement, Cartesian movements. Try to
+% implement joystick/gamepad implementation.
+
+% 3. Functional e-stop. Must be able to resume after e-stop. using uiwait() 
+% NOT allowed. 
+
+% 4. Simulate environment including safety hardware. Have active sensors (BONUS)
+
+% 5. Safety: Prevent Collisions (stop when 'simulated' object is placed in 
+% path of arm trajectory), To make one robot retreat from a simulated safety 
+% symbol that is held by the second robot using visual servoing and RMRC
+
+%%
 clc
 clf
 clear all
+startup_rvc;
 
-%%setup
+%% Setup
 scale1 = 0.3;
 scale2 = 0.5;
 workspace = [-1.5 1.5 -1.5 1.5 -0.65 1.5];
 
-%% UR5 DH
-UR5name = ['UR5'];
+%% Call robots
+hold on;
 
-LU1 = Link('d',0.0892,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-LU2 = Link('d',0.1357,'a',0.425,'alpha',-pi,'offset',-pi/2,'qlim',[deg2rad(-90),deg2rad(90)]);
-LU3 = Link('d',0.1197,'a',0.39243,'alpha',pi,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
-LU4 = Link('d',0.093,'a',0,'alpha',-pi/2,'offset',-pi/2,'qlim',[deg2rad(-360),deg2rad(360)]);
-LU5 = Link('d',0.093,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-LU6 = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+UR5bot = UR5arm;
+Reactorbot = Reactorx200;
 
-UR5arm = SerialLink([LU1 LU2 LU3 LU4 LU5 LU6], 'name',UR5name);
-qur5 = zeros(1,6);
-UR5arm.base = UR5arm.base * transl(0,0.5,0);
+%Move robot bases
+UR5bot.model.base = transl([0,0.3,0])* trotz(pi);
+UR5bot.MoveRobot(UR5bot.model.getpos);
 
-%% Reactorx 200 DH
-Reactorx200name = ['Reactorx200'];
+Reactorbot.model.base = transl([0,-0.3,0])* trotz(pi);
+Reactorbot.MoveRobot(Reactorbot.model.getpos);
 
-L1 = Link('d',0.10391,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
-L2 = Link('d',0,'a',0.200,'alpha',0,'offset',0,'qlim',[deg2rad(-108),deg2rad(113)]);
-L3 = Link('d',0,'a',0.050,'alpha',0,'offset',0,'qlim',[0]);
-L4 = Link('d',0,'a',0.200,'alpha',0,'offset',0,'qlim',[deg2rad(-108),deg2rad(93)]);
-L5 = Link('d',0,'a',0.065,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-100),deg2rad(123)]);
-L6 = Link('d',0,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',[0]);
-L7 = Link('d',0.065,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
-
-Reactorx200 = SerialLink([L1 L2 L3 L4 L5 L6 L7], 'name',Reactorx200name);
-Reactorx200.base = Reactorx200.base * transl(0,-0.5,0);
-qrea = zeros(1,7);
+% Reactorx200name = ['Reactorx200'];
+% 
+% L1 = Link('d',0.10391,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
+% L2 = Link('d',0,'a',0.200,'alpha',0,'offset',0,'qlim',[deg2rad(-108),deg2rad(113)]);
+% L3 = Link('d',0,'a',0.050,'alpha',0,'offset',0,'qlim',[0]);
+% L4 = Link('d',0,'a',0.200,'alpha',0,'offset',0,'qlim',[deg2rad(-108),deg2rad(93)]);
+% L5 = Link('d',0,'a',0.065,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-100),deg2rad(123)]);
+% L6 = Link('d',0,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',[0]);
+% L7 = Link('d',0.065,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
+% 
+% Reactorx200 = SerialLink([L1 L2 L3 L4 L5 L6 L7], 'name',Reactorx200name);
+% Reactorx200.base = Reactorx200.base * transl(0,-0.5,0);
+% qrea = zeros(1,7);
 
 %% plot
-UR5arm.plot(qur5, 'scale', scale2,'workspace',workspace)
-hold on;
-Reactorx200.plot(qrea, 'scale', scale1, 'workspace',workspace)
-
+% Reactorx200.plot(qrea, 'scale', scale1, 'workspace',workspace)
+% hold on;
+% UR5arm.plot(qur5, 'scale', scale2,'workspace',workspace)
+% UR5bot.MoveRobot(UR5bot.model.getpos);
 %% ply files
 ChessPieces
 
---
+++ UR5arm/UR5arm.m
@@ -0,0 +1,80 @@
+classdef UR5arm < handle
+    properties
+        model
+        name = 'UR5bot'
+        workspace = [-1 2 -2 2 -1.5 2];
+
+        % Robot plot options
+        plotopts = {'fps',60,'noarrow'};
+                
+        %default joint settings
+        jointDef = [90 0 0 0 0 0]*pi/180;
+    end
+
+    methods
+        %% Define Robot Function
+        function self = UR5arm(option)
+            self.CreateModel();
+            self.PlotAndColourRobot();
+            self.model.animate(self.jointDef)
+        end
+
+        %% DH Parameters
+        function CreateModel(self)
+            L(1) = Link('d',0.0892,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L(2) = Link('d',0.1357,'a',0.425,'alpha',-pi,'offset',-pi/2,'qlim',[deg2rad(-90),deg2rad(90)]);
+            L(3) = Link('d',0.1197,'a',0.39243,'alpha',pi,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
+            L(4) = Link('d',0.093,'a',0,'alpha',-pi/2,'offset',-pi/2,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L(5) = Link('d',0.093,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L(6) = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+
+            self.model = SerialLink(L,'name',self.name);
+        end
+
+        %% Plot and Colour Robot
+        function PlotAndColourRobot(self)
+            for linkIndex = 0:self.model.n
+                [faceData, vertexData, plyData{linkIndex+1} ] = plyread(['UR5Link',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+                self.model.faces{linkIndex+1} = faceData;
+                self.model.points{linkIndex+1} = vertexData;
+            end
+
+            % Display robot
+            self.model.plot3d(zeros(1,self.model.n),self.plotopts{:},'workspace',self.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end
+            self.model.delay = 0;
+
+            % Colour 
+            for linkIndex = 0:self.model.n
+                handles = findobj('Tag', self.model.name);
+                h = get(handles,'UserData');
+                try 
+                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                                  , plyData{linkIndex+1}.vertex.green ...
+                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
+                    h.link(linkIndex+1).Children.FaceColor = 'interp';
+                catch ME_1
+                    disp(ME_1);
+                    continue;
+                end
+            end
+        end
+
+        %% Move robot function
+        function MoveRobot(self,qTrajectory)
+            for i=1:numrows(qTrajectory)
+                self.model.animate(qTrajectory(i,:));
+                drawnow()
+            end
+        end
+
+%         %% Test the movement of robot
+%         function TestMovement(self)
+%             qTest = rand(1,5);
+%             qTrajectory = jtraj(self.jointDefault,qTest,100);
+%             self.MoveRobot(qTrajectory);
+%         end
+    end
+end
